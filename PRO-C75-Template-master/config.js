bb8630cfa36542dff65e9381503a1405757138c2
AnyaLuk <76762756+AnyaLuk@users.noreply.github.com>
2022-10-11 19:47:20 -0700

    Add files via upload

---
+++ PRO-C75-Template-master/config.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAXJbjTyWC4mjWqzbTBgXsEvVwEcVMVZkM",
+  authDomain: "e-ride-95b32.firebaseapp.com",
+  projectId: "e-ride-95b32",
+  storageBucket: "e-ride-95b32.appspot.com",
+  messagingSenderId: "798853076777",
+  appId: "1:798853076777:web:b3809f31cc970f9b890938"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
